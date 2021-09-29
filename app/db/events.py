3d22e0dd03a4b3a6e67348f688a432bca78ff5a9
Tarequzzaman <tareqcse12@gmail.com>
2021-09-29 17:38:12 +0600

    violation error try to slove

--- app/db/events.py
+++ app/db/events.py
@@ -8,8 +8,6 @@ MYSQL_CONFIG = {
     k.replace("MYSQL_", ""): v for k, v in os.environ.items() if "MYSQL_" in k
 }
 
-print(MYSQL_CONFIG)
-
 async def connect_to_db(app: FastAPI) -> None:
     app.state.db = await aiomysql.create_pool(**MYSQL_CONFIG)
     print("connection complete")
@@ -17,4 +15,3 @@ async def connect_to_db(app: FastAPI) -> None:
 async def close_db_connection(app: FastAPI) -> None:
     app.state.db.close()
     print("connection closed")
-    
